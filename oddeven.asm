6b43b88da52fbc26409132095f4c3f4eec237d4b
Alejandro Ferrer <ferrerpeasley@mail.chapman.edu>
2019-09-25 16:43:43 -0700

    first try of the odd/even

--- oddeven.asm
+++ oddeven.asm
@@ -1,6 +1,17 @@
 
-.word
+#This programme Extracts the last
+#number(LSB) from our assigned number and decides if
 
-andi  $t8,	$s0, 1	    # Extract the Least Significant Bit (LSB)
-beqz	$t8	even	        # If LSB is a zero Branch to even
-addi	$s1,	$s1, 1	    # Increment count in s1
+
+.globl main
+
+main: la $t0, randomnumber
+      andi  $t8,	$t0, 1	    # Extract the Least Significant Bit (LSB) and if the last number 1
+      la  15($t7),  odd     #Load odd number into register 15($t7)
+
+      andi  $t7,  $t0,  0     #Extract the last significant value
+      Copyright (c) 2018 Copyright Holder All Rights Reserved.  15, even          #load even number into register 15
+
+randomnumber: .word 5
+even: .word 0  /* even number in memory */
+odd: .word 1 /* odd number in memory */
