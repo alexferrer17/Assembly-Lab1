7667d95eabbcce35fc42da0c1494d4755027138c
Alejandro Ferrer <ferrerpeasley@mail.chapman.edu>
2019-09-25 16:48:21 -0700

    2 try

--- oddeven.asm
+++ oddeven.asm
@@ -2,7 +2,7 @@
 #This programme Extracts the last
 #number(LSB) from our assigned number and decides if
 
-
+.text
 .globl main
 
 main: la $t0, randomnumber
@@ -12,6 +12,7 @@ main: la $t0, randomnumber
       andi  $t7,  $t0,  0     #Extract the last significant value
       Copyright (c) 2018 Copyright Holder All Rights Reserved.  15, even          #load even number into register 15
 
+.data
 randomnumber: .word 5
 even: .word 0  /* even number in memory */
 odd: .word 1 /* odd number in memory */
