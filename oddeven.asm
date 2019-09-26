aff3440e01929908205678c6ec35cc50e0092078
Alejandro Ferrer <ferrerpeasley@mail.chapman.edu>
2019-09-25 17:30:27 -0700

    4¡3rd try

--- oddeven.asm
+++ oddeven.asm
@@ -5,12 +5,15 @@
 .text
 .globl main
 
-main: la $t0, randomnumber
-      andi  $t8,	$t0, 1	    # Extract the Least Significant Bit (LSB) and if the last number 1
-      la  15($t7),  odd       #Load odd number into register 15($t7)
+main:
+        la $t1, randomnumber
+        andi  $t2,	$t0, 1	    # Extract the Least Significant Bit (LSB) and if the last number 1
+        la  15($t7),  odd       #Load odd number into register 15($t7)
+        andi  $t3,  $t0,  0     #Extract the last significant value
+        la  15, even          #load even number into register 15
 
-      andi  $t7,  $t0,  0     #Extract the last significant value
-      la  15, even          #load even number into register 15
+        li $v0, 10
+        syscall
 
 .data
 randomnumber: .word 5
